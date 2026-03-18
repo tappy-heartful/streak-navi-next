036bfde8517466385d3ab3fda6e165f40e1ebf9d
MellowZtmy <tappynanakoro@gmail.com>
2026-03-18 11:10:32 +0900

    theme_color修正

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     scope: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#2e7d32',
+    theme_color: '#000000ff',
     icons: [
       {
         src: 'https://tappy-heartful.github.io/streak-images/navi/favicon.png',
